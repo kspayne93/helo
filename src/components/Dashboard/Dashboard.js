e95f1f8f5fb6be5742c59d80cd4fcb852ef8d1b8
kspayne93 <kyle@investavante.com>
2019-01-11 15:53:34 -0700

    basic dashboard styling

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
 import Nav from '../../components/Nav/Nav';
+import './Dashboard.css';
 
 class Dashboard extends Component {
+   constructor() {
+      super();
+
+      this.state = {
+         searchInput: '',
+         userposts: true
+      }
+   }
    render() {
       return (
-         <div>
+         <div className='dash'>
             < Nav />
-            Dashboard
+            <div className='dash-filter-box'>
+               <div className='dash-search-box'>
+                  <input className='dash-search-bar' onChange={(e) => this.setState({ searchInput: e.target.value })} type="text"/>
+                  <button></button>
+               </div>
+
+               <div className='checkbox-container'>
+                  <p> My posts </p>
+                  <input type="checkbox"/>
+               </div>
+            </div>
+            <div className='post-container'></div>
          </div>
       );
    }
